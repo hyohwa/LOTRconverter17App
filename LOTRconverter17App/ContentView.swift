7e44aa0641795dc0a85fde51c1504584a0d0d40d
hyohwako <hyohwa.ko@ldi.co.jp>
2024-01-09 10:34:28 +0900

    Initial Commit

---
+++ LOTRconverter17App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LOTRconverter17App
+//
+//  Created by ldi_hyohwako on 2024/01/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRconverter17App/LOTRconverter17AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRconverter17AppApp.swift
+//  LOTRconverter17App
+//
+//  Created by ldi_hyohwako on 2024/01/09.
+//
+
+import SwiftUI
+
+@main
+struct LOTRconverter17AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
